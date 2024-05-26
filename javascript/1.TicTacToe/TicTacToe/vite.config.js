d669d3a76c456f415ebdf4fd8eb3f70e807004cc
steve <stephenmarri7@gmail.com>
2024-05-25 20:11:52 -0500

    changed base

--- javascript/1.TicTacToe/TicTacToe/vite.config.js
+++ javascript/1.TicTacToe/TicTacToe/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/100-Projects/javascript/1.TicTacToe/TicTacToe/',
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
