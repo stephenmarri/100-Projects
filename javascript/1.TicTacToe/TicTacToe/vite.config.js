d67c6dc865f0263c925154f67416c6e7b6a9c761
steve <stephenmarri7@gmail.com>
2024-05-25 20:11:01 -0500

    gh-pages added

--- javascript/1.TicTacToe/TicTacToe/vite.config.js
+++ javascript/1.TicTacToe/TicTacToe/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/100-Projects/javascript/1.TicTacToe/TicTacToe/',
   plugins: [react()],
 })
