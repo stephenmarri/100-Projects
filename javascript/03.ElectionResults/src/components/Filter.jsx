08e3d5ac866da003dd418ea8202793e5ee4c0aad
steve <stephenmarri7@gmail.com>
2024-06-13 16:14:17 -0500

    js3 - aligment fixed - please dont touch it again

--- javascript/03.ElectionResults/src/components/Filter.jsx
+++ javascript/03.ElectionResults/src/components/Filter.jsx
@@ -5,17 +5,17 @@ const Filter = ({data}) => {
     console.log(data)
     
     return (
-        <div id='fitler_container' className='w-full flex flex-row justify-around border-b border-black py-2 px-3'>
+        <div id='fitler_container' className='w-full flex flex-row justify-between py-3 px-3'>
 
             <select className='border border-gray-200 rounded-md' name="cars" id="cars">
-                <option value="Nation">Nation</option>
-                <option value="State">State</option>
+                <option value="Nation">MPs</option>
+                <option value="State">MLAs</option>
             </select>
 
             <select className='border border-gray-200 rounded-md' name="cars" id="cars">
                 {
-                    data.map(x => {
-                        return <option value="{x}">{x}</option>
+                    data.map((x, idx) => {
+                        return <option key={idx} value="{x}">{x}</option>
                     })
                 }                
             </select>
