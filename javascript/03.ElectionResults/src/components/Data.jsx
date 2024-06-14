2e277a8668e44952e7c367d5d30c4af01100f7bc
steve <stephenmarri7@gmail.com>
2024-06-14 12:16:15 -0500

    js3 - mostly completed

--- javascript/03.ElectionResults/src/components/Data.jsx
+++ javascript/03.ElectionResults/src/components/Data.jsx
@@ -9,9 +9,9 @@ const Data = ({ data }) => {
                 <thead className=''>
                     <tr className='text-left'>
                         <th className='py-3 ps-1 w-1/10'>#</th>
-                        <th className='py-3 ps-1 w-1.5/15'>Const.</th>
-                        <th className='py-3 ps-1 w-2/5'>Elected</th>
-                        <th className='py-3 ps-1 w-2/5'>Party</th>
+                        <th className='py-3 ps-1 w-0155'>Const.</th>
+                        <th className='py-3 ps-1 w-2/6' >Elected</th>
+                        <th className='py-3 ps-1 w-2/6' >Party</th>
                     </tr>
                 </thead>
                 
@@ -19,10 +19,10 @@ const Data = ({ data }) => {
                 {
                     data.map((constituency, index) => {
                         return <tr className='border-b border-t' key={`${index}`}>
-                            <td className='ps-1'>{constituency.data_const_number}</td>
-                            <td className='ps-1'>{constituency.data_constituency}</td>
-                            <td className='ps-1'>{constituency.data_leading_candidate}</td>
-                            <td className='ps-1'>{constituency.data_leading_party}</td>
+                            <td Style={"width: 10%"} className='ps-1 '>{constituency.data_const_number}</td>
+                            <td Style={"width: 24%"} className='ps-1 '>{constituency.data_constituency}</td>
+                            <td Style={"width: 34%"} className='ps-1 '>{constituency.data_leading_candidate}</td>
+                            <td Style={"width: 32%"} className='ps-1 '>{constituency.data_leading_party}</td>
                         </tr>
                     })
                 }
